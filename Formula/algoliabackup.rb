fcb9c068f3c178a0b7d51e535e90f460e2540011
Aymeric Beaumet <hi@aymericbeaumet.com>
2016-12-18 23:34:09 +0100

    algoliabackup@0.0.1

--- Formula/algoliabackup.rb
+++ Formula/algoliabackup.rb
@@ -2,7 +2,7 @@ class Algoliabackup < Formula
   desc "Easily backup your Algolia indices"
   homepage "https://github.com/aymericbeaumet/algoliabackup.sh"
 
-  url "https://github.com/aymericbeaumet/algoliabackup.sh/archive/master.zip"
+  url "https://github.com/aymericbeaumet/algoliabackup.sh/archive/v0.0.1.zip"
   head "https://github.com/aymericbeaumet/algoliabackup.sh.git"
   bottle :unneeded
 
