8fb9e3de30c72277c288236249c718bdea71e35b
Aymeric Beaumet <hi@aymericbeaumet.com>
2016-12-18 22:39:33 +0100

    update algoliabackup formula

--- Formula/algoliabackup.rb
+++ Formula/algoliabackup.rb
@@ -2,10 +2,9 @@ class Algoliabackup < Formula
   desc "Easily backup your Algolia indices"
   homepage "https://github.com/aymericbeaumet/algoliabackup.sh"
 
-  url "https://raw.githubusercontent.com/aymericbeaumet/algoliabackup.sh/master/bin/algoliabackup"
+  url "https://github.com/aymericbeaumet/algoliabackup.sh/archive/master.zip"
   head "https://github.com/aymericbeaumet/algoliabackup.sh.git"
 
   depends_on "jq"
-
   bin.install "bin/algoliabackup"
 end
